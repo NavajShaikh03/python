7f2229fa7c12e0d9b97d158aaa00cd7f6c8dc72a
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-02-22 23:27:57 +0530

    Removed old college programs and updated stack concept

--- college Python/basic program/character.py
+++
@@ -1,5 +0,0 @@
-character = input("Enter a character:")
-if (character in 'aouie'or character in 'AIOUE'):
-    print(f"{character} is vowel")
-else:
-    print(f"{character} is consonant")
--- college Python/basic program/even_odd_3.py
+++
@@ -1,5 +0,0 @@
-number =int( input("Enter the number for checking even or odd: "))
-if(number % 2==0):
-    print(f"{number} is even number")
-else:
-    print(f"{number} is odd number " )
