7f1869a5c7a0aea7ea274d9baf2e77542ddf0d0e
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-03-06 23:44:50 +0530

    pactice

--- C_Python/basic program/maximum_No_4.py
+++ C_Python/basic program/maximum_No_4.py
@@ -9,3 +9,17 @@ elif(number2 >= number1 and number2 >=number3):
     print(f"{number2} is the maximum number")
 else:
     print(f"{number3} is the maximum number")
+    
+# onther method to find maximum number using function
+def find_maximum(a, b, c):
+    if (a >= b and a >= c):
+        return a
+    elif (b >= a and b >= c):
+        return b
+    else:
+        return c
+number1= int(input("Enter the number1:"))
+number2= int(input("Enter the number2:"))
+number3= int(input("Enter the number3:"))
+maximum_number = find_maximum(number1, number2, number3)
+print(f"{maximum_number} is the maximum number")    
