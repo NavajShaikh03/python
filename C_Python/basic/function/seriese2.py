0441e0b68bf0924b75f0c78d5cba6a7e2e307f63
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-03-06 23:46:49 +0530

    pactice
    git add C_Python/basic/function/seriese2.py
    git rebase --continue#

---
+++ C_Python/basic/function/seriese2.py
@@ -0,0 +1,16 @@
+def number(n):
+    value=1
+    for i in range(1,n):
+        print(value,end=" ")
+        value = value *2
+n = int(input("Enter the no. for 2 series:"))
+number(n)
+
+#
+def number(n):
+    value=1
+    for i in range(1,n):
+        print(value,end=" ")
+        value = value *2
+n = int(input("Enter the no. for 2 series:"))
+number(n)
