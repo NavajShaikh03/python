e56e2dfff76fa0f57c6d6a2d2f5ee12526762d02
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-03-10 12:15:22 +0530

    basic of list

---
+++ C_Python/list/basic_of_list.py
@@ -0,0 +1,23 @@
+# Python List – Complete Guide 
+# A list in Python is a collection of items stored in a single variable. It is one of the most used data structures in Python.
+
+# Lists are:
+#  Ordered
+#  Changeable (mutable)
+#  Allow duplicate values
+
+# Here are the Python List Methods in sequence (with names) 
+# Python List Methods
+
+# append()
+# clear()
+# copy()
+# count()
+# extend()
+# index()
+# insert()
+# pop()
+# remove()
+# reverse()
+# sort()
+
