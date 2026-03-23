ec960be0243942aa88c73aec0b2900b22f6036e4
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-03-23 15:58:17 +0530

    modify the code break list into duplicate and non duplicate number

--- C_Python/list/duplicateNo.py
+++ C_Python/list/duplicateNo.py
@@ -1,9 +1,15 @@
 numbers = [1,2,3,4,3,2,3,3,4,2,2,2,1,67,676,567,56]
+
+print("original list is : ",numbers)
 duplicate_number = []
+non_duplicate_number = []
 
 for i in  numbers:
     if numbers.count(i) > 1 and i not in duplicate_number:
         duplicate_number.append(i)
-        numbers.remove(i)
-print("duplicate number in list:",duplicate_number)
-print(" non duplicate number in list using set:",numbers)
+            
+    if i not in duplicate_number:
+        non_duplicate_number.append(i)
+        
+print(" \n  duplicate number in list:",duplicate_number)
+print(" \n non duplicate number in list:",non_duplicate_number)
