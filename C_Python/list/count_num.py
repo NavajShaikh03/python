f391b49a46368821ba28838218cd16bb5a1fc32c
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-03-23 15:05:38 +0530

    comment the code for understanding

--- C_Python/list/count_num.py
+++ C_Python/list/count_num.py
@@ -1,19 +1,19 @@
-n = int(input("Enter number of element for count:"))  # take 
+n = int(input("Enter number of element for count:"))  # take  input  from the user 
 numbers = []
 
 for i in range(n):
     num = input("Enter number:")
     numbers.append(num)
 
-lower = input("Enter lower number from list:")
-high = input("Enter the higher number from list:")
+lower = input("Enter lower number from list:")    # this variable is take the lower value 
+high = input("Enter the higher number from list:")   # this take the grater value from list 
 
 count =0
 for i in numbers:
     if lower < i < high:
         count +=1
         
-print("count btn in list:",count)
+print("count btn in list:",count)     # count the number the in btn l and h 
 
 # other way  built function
 
