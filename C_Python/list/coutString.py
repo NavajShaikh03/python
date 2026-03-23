75918a6056ee4656c003b9da1b9937933f177c2c
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-03-23 15:53:17 +0530

    commit the some line for understanding

--- C_Python/list/coutString.py
+++ C_Python/list/coutString.py
@@ -1,7 +1,7 @@
-string = ["hello","mom","dad","mdm","mdm","mdm","mdm","mdm","mdm"]
+string = ["hello","mom","dad","mdm","mdm","mdm","mdm","mdm","mdm"]   #  string ogf words
 
 count =0
-for i in string:
+for i in string:   # in sting if  first an last character are same then count will increase by 1
     if len(i) >=2:
         if i[0]==i[-1]:   # check first and last character and if they are same then count will increase by 1
             count +=1
