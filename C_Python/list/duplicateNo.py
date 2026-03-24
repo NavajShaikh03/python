9e1c3141f45f5ba52614071249ff9fccb6f5794f
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-03-24 13:10:32 +0530

    comment the line of code

--- C_Python/list/duplicateNo.py
+++ C_Python/list/duplicateNo.py
@@ -5,10 +5,10 @@ duplicate_number = []
 non_duplicate_number = []
 
 for i in  numbers:
-    if numbers.count(i) > 1 and i not in duplicate_number:
+    if numbers.count(i) > 1 and i not in duplicate_number:     # store the duplicate number in list and also check duplicate number present or not in list if not present then store in list otherwise ignore
         duplicate_number.append(i)
             
-    if i not in duplicate_number:
+    if i not in duplicate_number:   # store the non duplicate number in list and also check non duplicate number present or not in list if not present then store in list otherwise ingnore
         non_duplicate_number.append(i)
         
 print(" \n  duplicate number in list:",duplicate_number)
