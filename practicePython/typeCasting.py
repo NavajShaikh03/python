a72719dfcfce0b647f6da1dbf4f5521f4cd77056
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-04-02 23:52:59 +0530

    practice the the type casting

---
+++ practicePython/typeCasting.py
@@ -0,0 +1,22 @@
+# the conversion of one data type into the other data type is known as type casting 
+
+a = "1"    # string 
+b = "3"
+print(a+b)
+
+print(int(a) + int(b))
+
+# implicit Typecasting 
+
+c = 1.9 
+d = 8 
+
+print(c + d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# ===========
+
+ 
