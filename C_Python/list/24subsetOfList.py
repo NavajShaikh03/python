86e8744f8df9a99bdc5e6fc08f010ec91d56de9b
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-04-08 20:06:08 +0530

    partition of list

---
+++ C_Python/list/24subsetOfList.py
@@ -0,0 +1,20 @@
+lst = [1,2,3,4,5,6,7,8,9,10]    # list devide into sublist parttion 
+lst.sort()
+
+tem_sub_list = []    # this is tem list when the creat sub the clear it 
+
+allSublist = []
+for i in range(0,len(lst),2):
+    tem_sub_list.append(i)
+    for j in range(i+1,len(lst)):
+        tem_sub_list.append(j)
+        break
+    allSubList = allSublist + tem_sub_list
+    print(tem_sub_list)    
+    allSublist.append(tem_sub_list)
+    tem_sub_list.clear()             
+print(allSublist)   # this print but empty why not ??
+ 
+        
+        
+        
