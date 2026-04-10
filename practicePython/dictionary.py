8904af631e4f16e9d22a50b9bd881e86f6fca80d
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-04-10 22:31:55 +0530

    practice of dictionary

---
+++ practicePython/dictionary.py
@@ -0,0 +1,71 @@
+# Dictionary are ordered collection of data item.They store multiple item in single variable ,
+# Dictionary item are key-values pairs that are separated by commas and enclosed and within curly
+# brackets {}.
+dic = {
+    "name"  :"navaj",
+    "age"   :22,
+    "vilage":"Solapur",
+    "branch":"DS",
+    "CGPA"  :9.1,
+    "color" :"black",
+}
+print(dic)
+print(dic["name"])
+print(dic["age"])
+print(dic.get('name'))   #  not show the the error when not find the key return None
+# print(dic["name2"])      #   when the key are not found then get error 
+print(dic.keys())        # return the all key
+print(dic.values())
+
+
+for key in dic.keys():
+    print(f"The value corresponding to the key {key} is : {dic[key]}")
+    
+print(type(dic.items()))
+for key,value in dic.items():
+    print(f"the value of corresponding to the key {key}:{value}")
+    
+    
+## method of dictionary
+
+# employee performance
+ep1 = {
+    1:65,
+    2:35,
+    3:98,
+    4:78,
+    5:90,
+    6:89,
+    7:74,
+    8:89,
+    9:90,
+    10:78,
+}
+
+ep2 = {
+    11:80,
+    12:20,
+    13:30,
+    14:78,
+    15:60,
+}
+
+ep1.update(ep2)
+print(ep1)
+
+ep1.clear
+print(ep1)
+
+empty = {}
+print(type(empty))
+
+ep2.pop(12)      #  remove the key from dictionary
+print(ep2)
+
+ep2.popitem()   # remove the last element from list
+print(ep2)
+
+del ep1[15]     # delate the specific key
+print(ep1)
+
+del ep1 # delate the dictionary
