4a7ffbdf02c9c7a19276f9fdb18d1d11dbd71b4c
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-04-10 22:35:22 +0530

    question

---
+++ practicePython/excersice/excersice3.py
@@ -0,0 +1,3 @@
+# 1.Creat a program capable of displaying question to the user like KBC
+# 2. Use List data type to store the question and their correct ans .
+# 3. Display the final amount the person is taking home after playing the game.
