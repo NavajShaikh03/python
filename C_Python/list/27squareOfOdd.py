8101d662977793656a9d1ca8e3c912fdcedb1c70
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-04-20 23:37:03 +0530

    square of odd number in the list

---
+++ C_Python/list/27squareOfOdd.py
@@ -0,0 +1,10 @@
+# 27. Display square of odd numbers between 1 to 20 using list comprehension
+
+
+odd_square = [odd**2 for odd in range(1,21,2)]
+print(odd_square)
+
+# other way to solve this it 
+
+odd_no_square = [ odd*odd  for odd in range(1,21) if odd % 2 !=0 ]
+print(odd_no_square)
