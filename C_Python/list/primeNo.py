9cf7db01cc354410bea2a6d5d561a5bf9f349da9
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-04-25 14:40:53 +0530

    modifies the code

--- C_Python/list/primeNo.py
+++ C_Python/list/primeNo.py
@@ -1,5 +1,7 @@
 # 29. Display prime numbers within specified range using list comprehension
-
-primes = [n for n in nums 
-        if n > 1 and len([i for i in range(2, n) if n % i == 0]) == 0]
-print(primes)
+# nums = [1,2,3,4,68,8,9,8,95,43]
+# primes = [n for n in nums if n > 1 and len([i for i in range(2, n) if n % i == 0]) == 0]
+# print(primes)
+n=3
+check = len([i for i in range(2, n) if n % i == 0]) == 0   # check the condition number are prime or not if true its prime otherwise not prime
+print(check)                                               
