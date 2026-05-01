80e88c3575dd2f45d4a877ed5c053599c165905e
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-01 23:40:57 +0530

    finding the matched pairs of dictionary

---
+++ C_Python/dictionary/matchKeyValue.py
@@ -0,0 +1,28 @@
+# 6. WAP to match key values in two dictionaries.
+dict1 = {
+    "name" : "navaj",
+    "age"  : 22,
+    "color":"red",
+}
+
+dict2 ={
+    "name" : "navaj",
+    "age"  : 22,
+    "color":"black",
+}
+
+for value1 in dict1.values():
+    for value2 in dict2.values():
+        if value1 == value2:
+            print("matched values of dictionary is :",value1)
+
+# other way to 
+
+for item in dict1.items():   # find the keys-values pairs of both dictionary
+    if item in dict2.items():
+        print(item)
+        
+# other ways to find the pairs 
+
+matched_pairs  = dict(dict1.items() &  dict2.items())    # & → used for matching/intersection 
+print(matched_pairs)
