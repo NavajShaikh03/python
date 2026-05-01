3f747fe380aa69e99648309926a71931d43f245c
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-01 23:42:08 +0530

    sort the dictionary

---
+++ C_Python/dictionary/sortDict.py
@@ -0,0 +1,25 @@
+# Write a Python program to sort a list alphabetically in a dictionary
+
+
+dict = {
+    "name" : "navaj",
+    "age"  : 22,
+    "color": "red",
+    
+}
+
+print(sorted(dict))              # this print only key in assending order by default using key 
+                                # sorted is create a new dictionary and .sort change in the original list 
+print(sorted(dict.items()))      # arrange the using keys 
+
+ 
+dict_list = {                       # keys list 
+    "fruits": ["banana","Apple","mango"],   
+    "color" : ["red","blue","black","origen"],
+}
+key_sorted = sorted(dict_list.items())
+print(key_sorted)
+
+for key in dict_list:
+    dict_list[key].sort()
+print((dict_list))
