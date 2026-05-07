6a50ccea717ec1afb586172235ef9c15b0a7faf1
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-07 23:08:51 +0530

    practice the code revise

--- C_Python/dictionary/squareOfkeys.py
+++ C_Python/dictionary/squareOfkeys.py
@@ -3,5 +3,5 @@
 n = int(input("Enter the number which square upto n number:"))
 square_no= {}
 for i in range(1,n+1):
-    square_no.setdefault(i,i**2)   # add each keys and its square
+    square_no.setdefault(i,i**2)   # add each keys and its value in squ.
 print(square_no)
