c4820e089f38f812ec52a09318286a92b09f3031
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-09 11:28:22 +0530

    modify the code and improve code

--- C_Python/dictionary/emptyDict.py
+++ C_Python/dictionary/emptyDict.py
@@ -7,13 +7,20 @@ dict = {
     "marks":[],
 }
 
+key_with_value = {}
+empty_key = {}
+
 for only_value_key in dict:    # this code find the only key value pairs 
-    if dict[only_value_key]:
-        print(only_value_key,":",dict[only_value_key])
-        # print(dict.keys())
+    if dict[only_value_key]:   # this condition means take the key-value pairs because without value pairs are does not exits so
+        # print(only_value_key,":",dict[only_value_key])
+        key_with_value[only_value_key]=dict[only_value_key]
+    else:
+        empty_key[only_value_key] = dict[only_value_key]   # store the empty key
+print("key value pair is :",key_with_value)
+print("empty keys is :",empty_key)
 
-print("\n")
+# print("\n")
 
-for only_key in dict:   # this code key with empty value 
-    if not dict[only_key]:
-        print(only_key,":",dict[only_key])
+# for only_key in dict:   # this code key with empty value 
+#     if not dict[only_key]:
+#         print(only_key,":",dict[only_key])
