aaae94782de5aeeffd808545a296f9751f0e8595
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-11 22:58:54 +0530

    comment the line code for understanding

--- C_Python/string/calculateFrequ.py
+++ C_Python/string/calculateFrequ.py
@@ -5,8 +5,8 @@ visited = ""   # store the character which is already visited
 for ch in text:
     if ch not in visited:
         count =0 
-        for v in text:
-            if  ch ==v:
+        for v in text:           # take the one character and compare the first character 
+            if  ch ==v:          #compare the character and count
                 count +=1
             visited +=ch
         print(ch,"=",count)
