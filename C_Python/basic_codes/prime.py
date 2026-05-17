9ede5e8920fd81f7d110f5e6bf8929a15654ead6
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-17 23:46:27 +0530

    revise the code

--- C_Python/basic_codes/prime.py
+++ C_Python/basic_codes/prime.py
@@ -1,7 +1,13 @@
 number = int(input("Enter the number for check prime or not :"))
 for i in range(1,number-1):
     if number %  i == 0 or number % i != 0:
-        print("number is not prime")
-    
+        print("number is not prime")  
+    break
 print("number is prime")
- 
+
+number = int(input("Enter the number for check prime or not :"))
+for i in range(i,number-1):
+    if number % i == 0 or number % i !=0:
+        print("number is not prime")
+    break 
+print("number is prime")
