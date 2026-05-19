68870034cd4b00db6034634c5a42ff2f27be4927
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-19 14:56:35 +0530

    comment the code

--- C_Python/basic_codes/pattern/11.py
+++ C_Python/basic_codes/pattern/11.py
@@ -2,9 +2,8 @@ i=4
 while(i>=1):
     j=1
     while(j<=i):
-        print(j,end=" ")
+        print(j,end=" ")   # print the number with end of space 
         j+=1
     print("")
     i-=1
-    
     
