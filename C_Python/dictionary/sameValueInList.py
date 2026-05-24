90d7fd9cbe7645c54aa27783ab3cdd0c16881769
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-24 19:53:43 +0530

    remove the same element frome the list

---
+++ C_Python/dictionary/sameValueInList.py
@@ -0,0 +1,14 @@
+# 8. WAP to remove same element from list of values from dictionary.
+# 1:[11,12,13,11,5,12],2:[‘a’,’b’,’a’,’c’}
+
+
+dict = {
+    1 :[11,22,33,34,55,22,66,44,77],
+    2 :['a','b','c','a']
+}
+
+# new_dict = {}
+# for key in dict:
+#     unique_list =  set(dict[key])
+#     new_dict[key] = unique_list
+# print(new_dict)
