74f9a608d17fd4008ed590e6fcbaad237dedc662
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-05-29 22:07:47 +0530

    basic information of set and its method

---
+++ C_Python/Set/basicOfSet.py
@@ -0,0 +1,23 @@
+# Python Set:
+# A Set is a collection of unique elements.
+# Sets are written using curly braces {}.
+# Sets are mutable (elements can be added or removed).
+# Sets are unordered (elements have no fixed position).
+# Sets do not allow duplicate values.
+# Sets do not support indexing or slicing.
+# Sets are iterable, so they can be used with a for loop.
+# Set elements must be immutable (hashable), such as integers, strings, tuples, etc.
+# Lists, dictionaries, and other sets cannot be elements of a set because they are mutable.
+
+# Most Important Methods of set:
+# add() 
+# update()
+# remove()
+# discard()
+# pop()
+# clear()
+# copy()
+# union()
+# intersection()
+# difference()
+# symmetric_difference()
