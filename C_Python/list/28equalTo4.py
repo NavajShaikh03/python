b057b7d7ef7d4f596d260bb4fc8b9c05d5d61959
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-06-06 00:36:23 +0530

    change the somthing else in ouput content

--- C_Python/list/28equalTo4.py
+++ C_Python/list/28equalTo4.py
@@ -2,4 +2,4 @@
 
 lst = ["navaj","mujir","mahi","sam",]
 equal_to_4 = [word for word in lst if len(word) == 4 or len(word) == 3]     # take the word and compare the len with the word consider the len is 4 or 3
-print("this word of length equal to the 4 :",equal_to_4)
+print("this word of length equal to the 4 or 3 :",equal_to_4)
