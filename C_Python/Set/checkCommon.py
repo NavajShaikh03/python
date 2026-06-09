5e30085d0fb65ece13a4ccc9b31f17a5d66cf06c
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-06-09 22:23:56 +0530

    check the common element present or not

---
+++ C_Python/Set/checkCommon.py
@@ -0,0 +1,7 @@
+set1 = {23,5,34,45,23,23,4,43,3,2}
+set2 = {5,43,34,2,246,46,332,6,56}
+
+for i in set1:      # check the common element both set
+    if i in set2 :
+        print("common element:",i)
+    
