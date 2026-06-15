7b0bc96451a65cb4819d8a695a90a53c4aae4fd8
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-06-15 19:20:27 +0530

    comments the code

--- C_Python/list/charaTostring.py
+++ C_Python/list/charaTostring.py
@@ -1,5 +1,5 @@
 character = ["N","a","v","a","j"]
 new_string = ""
 for i in range(len(character)):
-    new_string=new_string+character[i]
+    new_string=new_string+character[i]   #  take the index of character and add the string variable
 print(new_string)     
