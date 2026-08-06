473b6005e085342e5ee9bf83ba2e41c8e2746ecb
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-08-06 13:55:36 +0530

    count reapited modify the code and add the if condition statement

--- C_Python/string/countVowels.py
+++ C_Python/string/countVowels.py
@@ -6,7 +6,8 @@ found_vowels = []
 
 for char in text:
     if char in vowels:
-        count+=1
-        found_vowels.append(char)
+        if char not in found_vowels:
+            count+=1
+            found_vowels.append(char)
 print("vowels in text:",found_vowels)
 print("count of vowels is:",count)
