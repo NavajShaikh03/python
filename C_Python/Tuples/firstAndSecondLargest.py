2f0aa7b9acc8f728a251ce3d7f59d078191f2d27
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-08-12 17:54:13 +0530

    modfy the code

--- C_Python/Tuples/firstAndSecondLargest.py
+++ C_Python/Tuples/firstAndSecondLargest.py
@@ -1,12 +1,13 @@
 # Given a tuple of integers, find the first and second largest elements in one traversal.
 
-tup = (45,36,35,35,36,98,75,38,100)
+tup = (45,36,35,35,36,98,75,989,38,100,112,8374)
 first_largest = 0
 second_largest =0
 for i in tup:
     if i > first_largest:
+        second_largest = first_largest
         first_largest = i
-    else:
-        second_largest=first_largest
+    elif(i > second_largest):
+        second_largest=i
 print(first_largest)
 print(second_largest)
