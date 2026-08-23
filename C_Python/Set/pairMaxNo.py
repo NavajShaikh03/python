8f98e526b515bab4b5988734da040aa3b8058fbe
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-08-23 23:26:53 +0530

    check the logic

--- C_Python/Set/pairMaxNo.py
+++ C_Python/Set/pairMaxNo.py
@@ -4,6 +4,7 @@ set_to_list = list(set1)
 
 previous_sum = 0
 max_pairs = []
+max_pairs_sum = 0
 for i in range(0,len(set1)) :
     for j in range(i+1,len(set1)):
         current_sum = set_to_list[i] + set_to_list[j]
@@ -11,6 +12,7 @@ for i in range(0,len(set1)) :
         if current_sum > previous_sum:
             previous_sum = current_sum
             max_pairs=([set_to_list[i] , set_to_list[j]])    # not store the always value store value in list temporary
+            max_pairs_sum = set_to_list[i] + set_to_list[j]
 print("max pairs from the arr:",max_pairs)
-print(i,j)
-print("max pairs addition is :",i+j)            
+print(max_pairs_sum)
+print("max pairs addition is :",set_to_list[i]+set_to_list[j])            # both i and j have the same indexing number when i reached the last indexing number 
