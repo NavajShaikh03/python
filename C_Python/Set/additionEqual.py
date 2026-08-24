c333fa6d135ac4702517a3e60f32f53b1983f611
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-08-24 22:53:23 +0530

    find the pairs of addition

---
+++ C_Python/Set/additionEqual.py
@@ -0,0 +1,21 @@
+# check the addition of equal to 100 give pairs of number 
+
+numbers = {10, 20, 30, 40, 50, 60, 70, 80}
+numbers_list = list(numbers)
+
+target_sum =int(input("enter the number :"))
+pairs =[]
+
+for i in range(0,len(numbers_list)):
+    for j in range(i+1,len(numbers_list)):
+        
+        pair_of_addition = numbers_list[i] + numbers_list[j]        
+        if pair_of_addition == target_sum :
+            pairs.append([numbers_list[i],numbers_list[j]])
+            
+    sameNo_addition = numbers_list[i] + numbers_list[i]
+    if sameNo_addition == target_sum:                      # this condition execute end of the loop it give the same number pairs addition 
+        pairs.append([numbers_list[i], numbers_list[i]])
+
+print(f"your number is {target_sum} pairs of addition is :",pairs)
+            
